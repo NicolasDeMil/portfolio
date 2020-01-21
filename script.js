546b563e8085b193a0b90db78e826236f77a5c23
Nicolas De Mil <nicodemil@hotmail.com>
2020-01-21 20:48:56 +0000

    tentative scroll function js

--- script.js
+++ script.js
@@ -5,6 +5,29 @@
 //   });
 // });
 
+//SCROLL
+$(document).ready(function () {
+  $('.scrolld').click(function(){
+  $("html, body").animate({ scrollTop: window.pageYOffset + $(window).height()}, 800);
+  return "hello";
+  });
+
+  // $('.up-arrow').click(function(){
+  // $("html, body").animate({ scrollTop: window.scrollTop(0), 300);
+  // });
+  $('.down-arrow-body').click(function(){
+  $("html, body").animate({ scrollTop: window.pageYOffset + $(window).height()}, 800);
+  return "hello";
+  });
+
+  $('.up-arrow-body').click(function(){
+  $("html, body").animate({ scrollTop: window.pageYOffset - $(window).height()*4}, 800);
+  return "hello";
+  });
+});
+
+
+//SLIDESHOW 
 
 var slideIndex = 1;
 showSlides(slideIndex);
